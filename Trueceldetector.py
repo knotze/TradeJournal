57ff3867dabb995d6cd77b62c1a6482185ea4281
knotze <leoccamp09@gmail.com>
2026-03-13 20:41:12 -0600

    Add files via upload

---
+++ Trueceldetector.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def main():
+    girlfriend = input("Do you have a girlfriend (y/n): ").lower()
+
+    if girlfriend == "y":
+        print("YOU ARE A ICKY FAKECEL")
+    else:
+        print("you are a truecel")
+
+
+if __name__ == "__main__":
+    main()
